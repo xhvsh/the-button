847b0852bb7f97149c5998fe711ce62f4b1deaf6
xhvsh <denkiewicz.janek@gmail.com>
2024-02-08 02:23:34 +0100

    Added button colors
    New colors for button in the menu
    Right now we have 8 colors:
    red, green, blue, yellow, cyan, pink, white and black!
    If you find any bugs please report them to me, thanks <3

--- script.js
+++ script.js
@@ -63,3 +63,98 @@ button.addEventListener('click', () => {
     }, 400)
   }
 })
+
+let menu = false
+let overlay = false
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+  if (menu) {
+    menu = false
+    overlay = false
+    document.querySelector('.menu').classList.remove('active')
+    document.querySelector('.overlay').classList.remove('active')
+  } else {
+    menu = true
+    overlay = true
+    document.querySelector('.menu').classList.add('active')
+    document.querySelector('.overlay').classList.add('active')
+  }
+})
+document.querySelector('.overlay').addEventListener('click', () => {
+  menu = false
+  overlay = false
+  document.querySelector('.menu').classList.remove('active')
+  document.querySelector('.overlay').classList.remove('active')
+})
+
+let redBtn = document.querySelector('.red')
+let greenBtn = document.querySelector('.green')
+let blueBtn = document.querySelector('.blue')
+let yellowBtn = document.querySelector('.yellow')
+let cyanBtn = document.querySelector('.cyan')
+let pinkBtn = document.querySelector('.pink')
+let whiteBtn = document.querySelector('.white')
+let blackBtn = document.querySelector('.black')
+
+let color = localStorage.getItem('color') || 'red'
+
+let buttons = [redBtn, greenBtn, blueBtn, yellowBtn, cyanBtn, pinkBtn, whiteBtn, blackBtn]
+
+buttons.forEach((e) => {
+  if (e.getAttribute('data-color') == color) {
+    e.classList.add('clicked')
+  } else {
+    e.classList.remove('clicked')
+  }
+})
+
+buttons.forEach((e) => {
+  e.addEventListener('click', () => {
+    buttons.forEach((btn) => {
+      btn.classList.remove('clicked')
+    })
+    e.classList.add('clicked')
+    color = e.getAttribute('data-color')
+    localStorage.setItem('color', color)
+    changeColor(color)
+  })
+})
+
+function changeColor(col) {
+  switch (col) {
+    case 'red':
+      document.documentElement.style.setProperty('--primary', '#f00')
+      document.documentElement.style.setProperty('--secondary', '#a00')
+      break
+    case 'green':
+      document.documentElement.style.setProperty('--primary', '#0f0')
+      document.documentElement.style.setProperty('--secondary', '#0a0')
+      break
+    case 'blue':
+      document.documentElement.style.setProperty('--primary', '#00f')
+      document.documentElement.style.setProperty('--secondary', '#00a')
+      break
+    case 'yellow':
+      document.documentElement.style.setProperty('--primary', '#ff0')
+      document.documentElement.style.setProperty('--secondary', '#aa0')
+      break
+    case 'cyan':
+      document.documentElement.style.setProperty('--primary', '#0ff')
+      document.documentElement.style.setProperty('--secondary', '#0aa')
+      break
+    case 'pink':
+      document.documentElement.style.setProperty('--primary', '#f0f')
+      document.documentElement.style.setProperty('--secondary', '#a0a')
+      break
+    case 'white':
+      document.documentElement.style.setProperty('--primary', '#fff')
+      document.documentElement.style.setProperty('--secondary', '#aaa')
+      break
+    case 'black':
+      document.documentElement.style.setProperty('--primary', '#333')
+      document.documentElement.style.setProperty('--secondary', '#000')
+      break
+  }
+}
+
+changeColor(color)
